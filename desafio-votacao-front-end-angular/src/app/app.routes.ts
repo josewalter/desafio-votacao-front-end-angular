7aec91b321205d8dc4fcbae899336459629523a5
josewalter <josewalter408@gmail.com>
2025-01-02 13:27:13 -0300

    Subindo a correção dos componentes que estavam dando erro por causa de alguns pacotes que tinha que ser baixado para o aplicativo funcionar.

--- desafio-votacao-front-end-angular/src/app/app.routes.ts
+++ desafio-votacao-front-end-angular/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { ClienteComponent } from '../cliente/cliente.component';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    'path': '', component:ClienteComponent
+
+  },
+  {
+    'path': 'footer', component:FooterComponent
+  },
+  {
+    'path': 'header', component:HeaderComponent
+  }
+];
